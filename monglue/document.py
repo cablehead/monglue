ea3911b32ec53441b1f29cbd2e4cc086204dda7a
Andy Gayton <andy@vntr.us>
2010-12-19 19:01:28 -0800

    add validation on set

--- monglue/document.py
+++ monglue/document.py
@@ -1,6 +1,39 @@
+"""
+Exceptions
+"""
+class DocumentError(Exception):
+    """Generic Monglue Document exception"""
+
+
+class ValidationError(DocumentError):
+    """Document vaidation failed"""
+
+
+"""
+Validators
+"""
+def required(document, field_name):
+    return field_name in document
+
+def optional(document, field_name):
+    return True
+
+def _validate(klass, document):
+    if hasattr(klass, '__collection_fields__'):
+        validators = klass.__collection_fields__
+        for key in document:
+            if key not in validators:
+                raise ValidationError('Unknown field: %s' % key)
+
+        for key in validators:
+            if not validators[key](document, key):
+                raise ValidationError('Validation failed for: %s' % key)
+
+
 class Document(dict):
     @classmethod
     def new(klass, database, document):
+        _validate(klass, document)
         c = database[klass.__collection_name__]
         _id = c.insert(document)
         return klass(document)
@@ -11,5 +44,6 @@ class Document(dict):
 
     def set(self, database, document):
         self.update(document)
+        _validate(self, self)
         return database[self.__collection_name__].update(
             {'_id': self['_id']}, {'$set': document})
--- monglue/test/test_document.py
+++ monglue/test/test_document.py
@@ -2,6 +2,9 @@ import unittest
 
 from monglue.test.test_mongo import PyMongoStub
 from monglue.document import Document
+from monglue.document import required
+from monglue.document import optional
+from monglue.document import ValidationError
 
 
 class User(Document):
@@ -10,29 +13,65 @@ class User(Document):
         return '%s %s.' % (self['first_name'], self['last_name'][0])
 
 
+class UserStrict(User):
+    __collection_fields__ = {
+        'first_name': required,
+        'last_name': required,
+        'age': optional,
+    }
+
+
 class DoumentTest(unittest.TestCase):
+    def _get_database(self):
+        return PyMongoStub()['foo']
+
     def test_new(self):
-        _db = PyMongoStub()['foo']
-        u = User.new(_db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
+        db = self._get_database()
+        u = User.new(db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
         self.assertEqual(u.truncate_name(), 'Daniel H.')
         self.assertTrue('_id' in u)
 
     def test_find(self):
-        _db = PyMongoStub()['foo']
-        User.new(_db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
-        User.new(_db, {'first_name': 'Andy', 'last_name': 'Gayton'})
-        got = User.find(_db)
+        db = self._get_database()
+        User.new(db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
+        User.new(db, {'first_name': 'Andy', 'last_name': 'Gayton'})
+        got = User.find(db)
         got.sort(lambda x,y: cmp(x['first_name'], y['first_name']))
         self.assertEqual(
             [u.truncate_name() for u in got], ['Andy G.', 'Daniel H.'])
 
     def test_set(self):
-        _db = PyMongoStub()['foo']
-        u = User.new(_db, {'first_name': 'Ted', 'last_name': 'Burns'})
+        db = self._get_database()
+        u = User.new(db, {'first_name': 'Ted', 'last_name': 'Burns'})
         _id = u['_id']
-        u.set(_db, {'first_name': 'Ned'})
+        u.set(db, {'first_name': 'Ned'})
         self.assertEqual(
             u, {'_id': _id, 'first_name': 'Ned', 'last_name': 'Burns'})
         self.assertEqual(
-            User.find(_db),
+            User.find(db),
             [{'_id': _id, 'first_name': 'Ned', 'last_name': 'Burns'}])
+
+    def test_validation(self):
+        db = self._get_database()
+        u = UserStrict.new(
+            db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
+
+    def test_validation_required(self):
+        db = self._get_database()
+        self.assertRaises(
+            ValidationError,
+            UserStrict.new, db, {'first_name': 'Daniel'})
+
+    def test_validation_not_optional(self):
+        db = self._get_database()
+        self.assertRaises(
+            ValidationError,
+            UserStrict.new, db, {'address': '915 Hampshire St, San Francisco'})
+
+    def test_validation_on_set(self):
+        db = self._get_database()
+        u = UserStrict.new(
+            db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
+        self.assertRaises(
+            ValidationError,
+            u.set, db, {'address': '915 Hampshire St, San Francisco'})
