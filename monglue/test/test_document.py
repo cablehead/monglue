17289d178da9e9d21bd865f1d756b2cd3ad6802c
Andy Gayton <andy@vntr.us>
2010-12-19 18:35:44 -0800

    clean up tests a little

--- monglue/test/test_document.py
+++ monglue/test/test_document.py
@@ -11,30 +11,28 @@ class User(Document):
 
 
 class DoumentTest(unittest.TestCase):
-    def setUp(self):
-        self._db = PyMongoStub()['foo']
-
     def test_new(self):
-        u = User.new(
-            self._db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
+        _db = PyMongoStub()['foo']
+        u = User.new(_db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
         self.assertEqual(u.truncate_name(), 'Daniel H.')
         self.assertTrue('_id' in u)
 
     def test_find(self):
-        User.new(self._db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
-        User.new(self._db, {'first_name': 'Andy', 'last_name': 'Gayton'})
-        got = User.find(self._db)
+        _db = PyMongoStub()['foo']
+        User.new(_db, {'first_name': 'Daniel', 'last_name': 'Hengeveld'})
+        User.new(_db, {'first_name': 'Andy', 'last_name': 'Gayton'})
+        got = User.find(_db)
         got.sort(lambda x,y: cmp(x['first_name'], y['first_name']))
         self.assertEqual(
-            [u.truncate_name() for u in got],
-            ['Andy G.', 'Daniel H.'])
+            [u.truncate_name() for u in got], ['Andy G.', 'Daniel H.'])
 
     def test_set(self):
-        u = User.new(self._db, {'first_name': 'Ted', 'last_name': 'Burns'})
+        _db = PyMongoStub()['foo']
+        u = User.new(_db, {'first_name': 'Ted', 'last_name': 'Burns'})
         _id = u['_id']
-        u.set(self._db, {'first_name': 'Ned'})
+        u.set(_db, {'first_name': 'Ned'})
         self.assertEqual(
             u, {'_id': _id, 'first_name': 'Ned', 'last_name': 'Burns'})
         self.assertEqual(
-            User.find(self._db),
+            User.find(_db),
             [{'_id': _id, 'first_name': 'Ned', 'last_name': 'Burns'}])
