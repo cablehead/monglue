b353c0441a6012af65e95bf5e72406de2bd8d11a
Daniel Hengeveld <danielwh@gmail.com>
2011-01-15 22:39:58 -0800

    fix up setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Monglue',
+    name='monglue',
     version='0.0a',
+    packages=find_packages(),
     description='Minimal MongoDB document manager and toolkit',
     url='http://github.com/thedaniel/monglue',
     )
